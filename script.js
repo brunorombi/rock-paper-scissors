4d497d651897426e30cb11217fbe8274056af584
Bruno Rombi <brunorombi@yahoo.com>
2024-11-12 16:04:15 -0300

    Implement playRound function

--- script.js
+++ script.js
@@ -24,4 +24,36 @@ function getHumanChoice() {
     return getHumanChoice();
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`Draw, both choose ${humanChoice}`);
+        humanScore++;
+        computerScore++;
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        console.log(`You lose! Scissors beats Paper`);
+        computerScore++;
+    } else if (humanChoice === 'scissors' &&  computerChoice === 'paper') {
+        console.log(`You win! Scissors beats paper`);
+        humanScore++;
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        console.log(`You lose, Paper beats Rock`);
+        computerScore++;
+    } else if (computerChoice === 'rock' && humanChoice === 'paper') { 
+        console.log(`You win, Paper beats Rock`);
+        humanScore++;
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log(`You win! Rock beats Scissors`)
+        humanScore++;
+    } else {
+        console.log(`You lose, Rock beats Scissors`);
+        computerScore++;
+    }
+
+}
+
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
